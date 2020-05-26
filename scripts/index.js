8dd43fa206d282f8e6431a5848a8825150333226
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2020-05-25 20:47:20 -0600

    checkpoint

--- scripts/index.js
+++ scripts/index.js
@@ -44,26 +44,26 @@ function closeSidebar() {
 var slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function plusSlides(x) {
+    showSlides(slideIndex += x);
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function currentSlide(x) {
+    showSlides(slideIndex = x);
 }
 
-function showSlides(n) {
+function showSlides(x) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    var img_circle = document.getElementsByClassName("img_circle");
+    if (x > slides.length) { slideIndex = 1 }
+    if (x < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < img_circle.length; i++) {
+        img_circle[i].className = img_circle[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    img_circle[slideIndex - 1].className += " active";
 }
