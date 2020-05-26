b2cf1f8477e774cc9b46602c3768bb023a899774
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2020-05-25 23:18:00 -0600

    checkpoint

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,10 @@ var close = document.getElementsByClassName("close")[0];
 var side = document.getElementById("side");
 var main = document.getElementById("main");
 var slideIndex = 1;
-let myRequest = new Request("./text.json");
 
 popupLink.onclick = function () {
     popup.style.display = "block";
+    document.getElementById('modal-text').innerHTML = quote();
 }
 
 close.onclick = function () {
@@ -22,6 +22,7 @@ window.onclick = function (event) {
 
 function toast() {
     var message = document.getElementById("toast-message");
+    message.innerHTML = quote();
     message.className = "show";
     setTimeout(function () {
         message.className = message.className.replace("show", "");
@@ -64,10 +65,9 @@ function showSlides(x) {
     img_circle[slideIndex - 1].className += " active";
 }
 
-fetch(myRequest)
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-        console.log(data.quotes)
-    })
+function quote() {
+    var obj = JSON.parse('{"quotes": [ "“Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.” ~Dr. Seuss", "“Do what you feel in your heart to be right, for you’ll be criticized anyway.” ~Eleanor Roosevelt", "“To find yourself, think for yourself.” ~Socrates", "“Two things are infinite: the universe and human stupidity; and I’m not sure about the universe.” ~Albert Einstein", "“You only live once, but if you do it right, once is enough.” ~Mae West", "“Be the change that you wish to see in the world.” ~Mahatma Gandhi", "“No one can make you feel inferior without your consent.” ~Eleanor Roosevelt", "“Always forgive your enemies; nothing annoys them so much.” ~Oscar Wilde", "“I have not failed. I’ve just found 10,000 ways that won’t work.” ~Thomas A. Edison", "“I like nonsense, it wakes up the brain cells. Fantasy is a necessary ingredient in living.” ~Dr. Seuss"]}')
+    var x = Math.floor((Math.random() * 10));
+    // console.log(obj.quotes[x]);   
+    return obj.quotes[x];
+}
